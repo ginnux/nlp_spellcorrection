52612cabfeb9dd9c084ba0e3211cb136228c2c38
komusama <1029673928@qq.com>
2024-06-04 14:36:32 +0800

    更新UI

--- UI/correction.py
+++ UI/correction.py
@@ -77,21 +77,35 @@ class spell_correction():
         self.V = len(self.term_count.keys())
 
     def correction(self, line):
+        # 分开逗号两边的词
         line = re.sub(r"([,])([^\d])", r" \1 \2", line)
-        line = re.sub(r"([^s])(['])", r"\1 \2", line)
-        line = re.sub(r"([s])(['])", r"\1 \2 ", line)
-        line = re.sub(r"([.]$)", r" \1 ", line)
 
-        line = line.split(' ')
+        # 单引号分开原则：特别注意s'和n't情况，以及作为成对引号
+        # 先考虑成对引号，第一个引号前一般是空格，展开这对单引号，两侧加入空格
+        line = re.sub(r"(\s)(['])([^']+)(['])", r"\1\2 \3 \4 ", line)
+        # 一般情况，一般来说单引号前是一个完整的单词
+        line = re.sub(r"([^sn\s])(['])", r"\1 \2", line)
+        # 若单引号前是复数s，单引号后是缩写，如s'，则不分开
+        line = re.sub(r"([s])(['])", r"\1 \2 ", line)
+        # 若单引号是n't的情况，如don't，won't等，则不应该分开n't，而应该分开n't两边的词，比如do
+        line = re.sub(r"([n])(['])([t])", r" \1\2\3 ", line)
+        # 若单引号是前是n，后面不是t，则就是一般情况，分开n和单引号
+        line = re.sub(r"([n])(['])([^t])", r"\1 \2\3", line)
+
+        # 识别句尾标点符号并分开
+        line = re.sub(r"([.?!]\s*$)", r" \1 ", line)
+
+        items = line.split("\t")
+        line = items[2].split()
         corrected_line = line
         j = 0
         # 遍历句子单词
         for word in line:
-            if word not in self.vocab:
+            if word not in vocab:
                 # 需要替换word成正确的单词
                 # Step1: 生成所有的(valid)候选集合
                 # 获得编辑距离小于2的候选列表
-                candidates = self.CG.generate_candidates(word, max_distance=self.max_distance)
+                candidates = CG.generate_candidates(word, max_distance=max_distance)
                 candidates = list(candidates)
                 probs = []
 
@@ -102,8 +116,8 @@ class spell_correction():
                 for candi in candidates:
                     prob = 0
                     # 计算channel probability
-                    if candi in self.channel_prob and word in self.channel_prob[candi]:
-                        prob += np.log(self.channel_prob[candi][word])
+                    if candi in channel_prob and word in channel_prob[candi]:
+                        prob += np.log(channel_prob[candi][word])
                     else:
                         prob += np.log(0.0001)
 
@@ -111,13 +125,19 @@ class spell_correction():
                     # 以s=I like playing football.为例line=['I','like','playing','football']
                     # word为playing时
                     if j > 0:
-                        forward_word = line[j - 1] + " " + candi  # 考虑前一个单词,出现like playing的概率
-                        prob += calculate_smoothed_probability(self.bigram_count, self.term_count, self.V, forward_word,
-                                                               line[j - 1])
+                        forward_word = (
+                                line[j - 1] + " " + candi
+                        )  # 考虑前一个单词,出现like playing的概率
+                        prob += calculate_smoothed_probability(
+                            bigram_count, term_count, V, forward_word, line[j - 1]
+                        )
                     if j + 1 < len(line):
-                        backward_word = candi + " " + line[j + 1]  # 考虑后一个单词，出现playing football的概率
-                        prob += calculate_smoothed_probability(self.bigram_count, self.term_count, self.V,
-                                                               backward_word, candi)
+                        backward_word = (
+                                candi + " " + line[j + 1]
+                        )  # 考虑后一个单词，出现playing football的概率
+                        prob += calculate_smoothed_probability(
+                            bigram_count, term_count, V, backward_word, candi
+                        )
                     probs.append(prob)
 
                 if probs:
@@ -128,15 +148,41 @@ class spell_correction():
                         corrected_line[j] = candidates[max_idx]
             j += 1
 
+        # 因为对所有词间加入了标点符号，要对句子标点符号修正
         corrected_sentence = " ".join(corrected_line)
-        corrected_sentence = re.sub(r"\s*(['])\s*", r"\1", corrected_sentence)  # 去除标点前的空格
-        corrected_sentence = re.sub(r"(s')", r"\1 ", corrected_sentence)  # 恢复s'的情况
 
-        corrected_sentence = re.sub(r"\s([.])\s", r"\1", corrected_sentence)  # 去除标点前的空格
+        # 先将单引号周围一侧的空格去掉，再按情况恢复，但两侧都有空格的是成对的引号，不去除
+        corrected_sentence = re.sub(r"\s+(['])([^\s])", r"\1\2", corrected_sentence)
+        corrected_sentence = re.sub(r"([^\s])(['])\s+", r"\1\2", corrected_sentence)
+        # 单引号周围是空格的情况一般只有s'，恢复
+        corrected_sentence = re.sub(r"(s')", r"\1 ", corrected_sentence)
+        # 去除成对单引号剩余的空格，前单引号后面的空格去掉，后单引号前面的空格去掉
+        corrected_sentence = re.sub(
+            r"(\s)(')(\s)([^']*)(\s)(')(\s)", r"\1\2\4\6\7", corrected_sentence
+        )
 
-        corrected_sentence = re.sub(r"\s([,])", r"\1", corrected_sentence)  # 去除标点前的空格（保留逗号后面的空格）
-        corrected_sentence = re.sub(r"(\d)([,])\s+(\d)", r"\1\2\3", corrected_sentence)  # 去除数据中的空格
-        # 句点补全
-        if corrected_sentence[-1] != ".":
+        # 句尾标点旁边的空格去除
+        corrected_sentence = re.sub(r"\s([.])\s", r"\1", corrected_sentence)
+
+        # 处理逗号附近的空格
+        # 逗号前面不可能有空格，去掉
+        corrected_sentence = re.sub(r"\s([,])", r"\1", corrected_sentence)
+        # 如果两边都有数字，则只有是数字中的逗号和年份月份分开的情况，先去除所有空格
+        corrected_sentence = re.sub(r"(\d)([,])\s+(\d)", r"\1\2\3", corrected_sentence)
+        # 恢复年份的逗号空格, 因为数字每三位要加逗号，年份一般是4位数
+        corrected_sentence = re.sub(
+            r"(\d{4,})([,])(\d)", r"\1\2 \3", corrected_sentence
+        )
+        corrected_sentence = re.sub(r"([,])(\d{4,})", r"\1 \2", corrected_sentence)
+
+        # 句点补全，句子原来有什么标点，就补全什么标点，否则就补句号
+        # 先去掉句尾空格
+        corrected_sentence = re.sub(r"([.?!])\s*$", r"\1", corrected_sentence)
+        # 补全
+        if (
+                corrected_sentence[-1] != "."
+                and corrected_sentence[-1] != "?"
+                and corrected_sentence[-1] != "!"
+        ):
             corrected_sentence += "."
         return corrected_sentence
